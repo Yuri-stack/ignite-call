5ad7daedd7e49303b7838ac228a447599226dc1f
Yuri-stack <yurioliveirasilva1999@gmail.com>
2023-12-25 13:15:49 -0300

    Config.: Configurando o EsLint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import type { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
 
-globalStyles()  // Tranzendo a config de CSS
+globalStyles() // Tranzendo a config de CSS
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+  return <Component {...pageProps} />
 }
