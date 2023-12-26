70de32b540ed4ffb4f7908ffd9c8b51440dbc879
Yuri-stack <yurioliveirasilva1999@gmail.com>
2023-12-25 22:03:01 -0300

    Feat.: Manipulando cookies no Next

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { setCookie } from 'nookies'
 import { prisma } from '@/src/lib/prisma'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse,) {
@@ -8,10 +9,24 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse,
 
     const { name, username } = req.body
 
+    const userExists = await prisma.user.findUnique({
+        where: { username }
+    })
+
+    if (userExists)
+        return res.status(400).json({
+            message: 'Usuário já cadastrado'
+        })
+
     const user = await prisma.user.create({
         data: {
             name, username
         }
+    })
+
+    setCookie({ res }, '@ignite-call:userId', user.id, {
+        maxAge: 60 * 60 * 24 * 7, // 7 dias
+        path: '/'
     })
 
     return res.status(201).json(user)
