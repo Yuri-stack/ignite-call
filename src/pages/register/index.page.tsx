f63fb6dc4e31f879f4fbfe4de9e1afbf05c12131
Yuri-stack <yurioliveirasilva1999@gmail.com>
2023-12-25 22:03:01 -0300

    Feat.: Manipulando cookies no Next

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -7,6 +7,7 @@ import { Container, Form, FormError, Header } from './styles'
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { api } from '@/src/lib/axios'
+import { AxiosError } from 'axios'
 
 const registerFormSchema = z.object({
     username: z
@@ -50,8 +51,11 @@ export default function Register() {
             })
 
         } catch (error) {
-            console.log(error)
-
+            if (error instanceof AxiosError && error?.response?.data?.message) {
+                alert(error.response.data.message)
+                return
+            }
+            console.error(error)
         }
     }
 
