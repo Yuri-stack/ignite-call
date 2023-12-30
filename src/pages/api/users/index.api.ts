54cfa6e5e25a2143706256f04cb41bd55eedc900
Yuri-stack <yurioliveirasilva1999@gmail.com>
2023-12-30 16:52:36 -0300

    Feat.: Persistindo intervalos

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,11 +1,10 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
 import { setCookie } from 'nookies'
 import { prisma } from '@/src/lib/prisma'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse,) {
-    if (req.method !== 'POST') {
+    if (req.method !== 'POST')
         return res.status(405).end()
-    }
 
     const { name, username } = req.body
 
