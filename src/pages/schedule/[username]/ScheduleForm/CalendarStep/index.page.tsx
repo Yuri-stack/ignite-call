4a44be9982f5804b56199921fc666408028b5e92
Yuri-stack <yurioliveirasilva1999@gmail.com>
2024-01-20 20:38:57 -0300

    Feat.: Adicionando a estrutura visual da lista de horários

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.page.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.page.tsx
@@ -1,10 +1,31 @@
 import { Calendar } from "@/src/components/Calendar";
-import { Container } from "./styles";
+import { Container, TimePicker, TimePickerHeader, TimePickerItem, TimePickerList } from "./styles";
 
 export function CalendarStep() {
+    const isDateSelected = true
+
     return (
-        <Container>
+        <Container isTimePickerOpen={isDateSelected}>
             <Calendar />
+
+            {isDateSelected && (
+                <TimePicker>
+                    <TimePickerHeader>
+                        terça-feira <span>20 de setembro</span>
+                    </TimePickerHeader>
+
+                    <TimePickerList>
+                        <TimePickerItem>8:00h</TimePickerItem>
+                        <TimePickerItem>8:00h</TimePickerItem>
+                        <TimePickerItem>8:00h</TimePickerItem>
+                        <TimePickerItem>8:00h</TimePickerItem>
+                        <TimePickerItem>8:00h</TimePickerItem>
+                        <TimePickerItem>8:00h</TimePickerItem>
+                        <TimePickerItem>8:00h</TimePickerItem>
+                        <TimePickerItem>8:00h</TimePickerItem>
+                    </TimePickerList>
+                </TimePicker>
+            )}
         </Container>
     )
 }
